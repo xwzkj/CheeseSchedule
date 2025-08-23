b778d1402287679c44fa8a05a393e047badeae6f
xwzkj <xwzkjbili@gmail.com>
2025-08-23 16:01:47 +0800

    首次提交

---
+++ src/component/classCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="p-0.5rem rounded-1rem m-0.1rem flex flex-col items-center w-7.5rem shadow-[0_0_3px_0_#777]"
+        :class="{ 'bg-#66ccff': props.active, 'bg-#eee': !props.active }">
+
+        <div class="font-bold" :class="{ 'text-2.5rem': props.active, 'text-2rem': !props.active }">
+            {{ props.name }}
+        </div>
+        <div v-if="props.active" class="text-1rem">{{ props.time }}</div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    name: string,
+    time: string | null,
+    active: boolean
+}>()
+</script>
+
+<style scoped></style>
