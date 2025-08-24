2a155c49a23a3d68a05cccf3b9db15c79ed9cc07
xwzkj <xwzkjbili@qq.com>
2025-08-24 19:42:05 +0800

    add:主页ui改进，加入过渡动画

--- src/component/classCard.vue
+++ src/component/classCard.vue
@@ -1,21 +1,51 @@
 <template>
-    <div class="p-0.2rem m-b-0.3rem flex flex-col items-center w-7.5rem rounded-4 border-#999 border-solid border-1"
-        :class="{ 'bg-#55efc4': props.active == 2, 'bg-#66ccff': props.active == 1, 'bg-white': props.active == 0 }">
+    <div ref="outer" class="outer" :class="{ 'w-7.5rem max-h-7rem': props.active, 'w-5.5rem max-h-5rem': !props.active }">
+        <div class="relative flex flex-col items-center z-2">
 
-        <div class="font-bold" :class="{ 'text-2.5rem': props.active, 'text-2rem': !props.active }">
-            {{ props.name }}
+            <div :class="{'text-2.5rem font-bold': props.active, 'text-1.8rem': !props.active }">
+                {{ props.name }}
+            </div>
+            <div v-if="props.active" class="text-1rem whitespace-nowrap">{{ props.time }}</div>
+
         </div>
-        <div v-if="props.active" class="text-1rem">{{ props.time }}</div>
-
+        <div class="absolute h-75% w-75% rounded-1rem bg z-1">
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { watch, useTemplateRef } from 'vue'
 const props = defineProps<{
     name: string,
     time: string | null,
     active: 0 | 1 | 2
 }>()
+const outerEle = useTemplateRef('outer')
+watch(() => props.active, (val) => {
+    if (outerEle.value) {
+        outerEle.value.style.setProperty('--bg-color', val == 0 ? 'white' : val == 1 ? '#66ccff' : '#55efc4');
+        // outerEle.value.style.setProperty('--bg-color', 'blue');
+    }
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.outer {
+    padding: 0.2rem;
+    margin-bottom: 0.3rem;
+    border-radius: 1rem;
+    border: 1px solid #ccc;
+    background-color: white;
+    position: relative;
+    overflow: hidden;
+    transition: all 0.5s;
+}
+
+.bg {
+    top: 12.5%;
+    left: 12.5%;
+    background-color: var(--bg-color, white);
+    box-shadow: 0 0 4rem 1rem var(--bg-color, white);
+    filter: blur(1rem);
+}
+</style>
