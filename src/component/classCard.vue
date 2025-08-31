29a229ff7e4b2b34c8b95a201e93631ba55a8ab4
xwzkj <xwzkjbili@qq.com>
2025-08-31 15:37:15 +0800

    add: 动画效果

--- src/component/classCard.vue
+++ src/component/classCard.vue
@@ -1,14 +1,15 @@
 <template>
-    <div ref="outer" class="outer" :class="{ 'w-7.5rem max-h-7rem': props.active, 'w-5.5rem max-h-5rem': !props.active }">
+    <div ref="outer" class="outer" :class="{ 'w-9rem h-5.7rem': props.active, 'w-5.5rem h-3.2rem': !props.active }">
         <div class="relative flex flex-col items-center z-2">
 
-            <div :class="{'text-2.5rem font-bold': props.active, 'text-1.8rem': !props.active }">
+            <div :class="{ 'text-2.5rem font-bold': props.active, 'text-1.8rem': !props.active }"
+                class="line-height-120%">
                 {{ props.name }}
             </div>
-            <div v-if="props.active" class="text-1rem whitespace-nowrap">{{ props.time }}</div>
+            <div v-if="props.active" class="text-1.3rem font-bold whitespace-nowrap">{{ props.time }}</div>
 
         </div>
-        <div class="absolute h-75% w-75% rounded-1rem bg z-1">
+        <div v-if="props.active" class="bg">
         </div>
     </div>
 </template>
@@ -38,14 +39,48 @@ watch(() => props.active, (val) => {
     background-color: white;
     position: relative;
     overflow: hidden;
-    transition: all 0.5s;
+    transition: all 0.7s ease-in-out;
+    /* 居中 */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 .bg {
-    top: 12.5%;
-    left: 12.5%;
+    z-index: 1;
+    height: 65%;
+    aspect-ratio: 1 / 1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     background-color: var(--bg-color, white);
     box-shadow: 0 0 4rem 1rem var(--bg-color, white);
-    filter: blur(1rem);
+    border-radius: 1rem;
+    filter: blur(0.7rem);
+    animation: bg-animation 10s linear infinite;
+}
+
+@keyframes bg-animation {
+    0% {
+        transform: translate(-75%, -50%) rotate(0deg) scale(1);
+    }
+
+    25% {
+        transform: translate(-50%, -50%) rotate(90deg) scale(1.2);
+    }
+
+    50% {
+        transform: translate(-25%, -50%) rotate(180deg) scale(1);
+    }
+
+    75% {
+        transform: translate(-50%, -50%) rotate(270deg) scale(1.2);
+    }
+
+    100% {
+        transform: translate(-75%, -50%) rotate(360deg) scale(1);
+    }
 }
 </style>
