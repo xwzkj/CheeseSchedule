e38786d921d704beccae21e31bab36add073c484
xwzkj <xwzkjbili@gmail.com>
2025-08-31 16:39:05 +0800

    add: 用自动完成组件替代下拉菜单，现在可以自定义课程名称了

--- src/component/daySchedule.vue
+++ src/component/daySchedule.vue
@@ -1,32 +1,36 @@
 <template>
     <div class="flex flex-col items-center gap-1">
-        <n-dropdown :options="scheduleStore.patternsOption" @select="handlePatternSelect">
+        <n-dropdown :options="scheduleStore.patternsOption" @select="handlePatternSelect" trigger="click">
             <n-button dashed type="primary">{{ patternName }}</n-button>
         </n-dropdown>
         <div class="flex flex-col items-center gap-1" v-if="scheduleStore.schedule[props.day]?.pattern != -1">
             <div v-for="(item, index) in scheduleStore.patterns?.[scheduleStore.schedule[props.day]?.pattern]?.data"
                 class="w-100%">
                 <n-divider v-if="item.isDivider" style="margin:3px 0;"></n-divider>
-                <n-dropdown v-else :options="lessonsOption" trigger="click"
-                    @select="(k) => handleLessonSelect(k, index)">
 
-                    <div
-                        class="p-1 rounded-2 border-#999 border-dashed border-1 flex items-center flex-col cursor-pointer">
-                        <div class="text-1.2rem">{{ scheduleStore.schedule[props.day]?.lessons?.[index]?.name ??
-                            '空' }}</div>
-                        <div class="text-0.7rem text-gray">{{ item?.time }}</div>
-                    </div>
+                <div v-else
+                    class="p-1 rounded-2 border-#999 border-dashed border-1 flex items-center flex-col cursor-pointer">
+                    <!-- <div class="text-1.2rem">{{ scheduleStore.schedule[props.day]?.lessons?.[index]?.name ??
+                            '空' }}</div> -->
+                    <n-auto-complete v-model:value="scheduleStore.schedule[props.day].lessons[index].name"
+                        :options="lessonsOption" :input-props="{
+                            autocomplete: 'disabled'
+                        }" blur-after-select class="w-4.5rem">
 
-                </n-dropdown>
+                    </n-auto-complete>
+                    <div class="text-0.7rem text-gray">{{ item?.time }}</div>
+                </div>
+
+
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { NDropdown, NButton, NDivider } from 'naive-ui'
+import { NDropdown, NAutoComplete, NButton, NDivider } from 'naive-ui'
 import { useScheduleStore } from '../stores/scheduleStore';
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 const scheduleStore = useScheduleStore();
 const props = defineProps<{
     day: "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun"
@@ -35,68 +39,16 @@ let patternName = computed(() => {
     let p = scheduleStore.schedule[props.day]?.pattern
     return p != -1 ? scheduleStore.patterns?.[p]?.name : "选择本日模式"
 })
-const lessonsOption = ref([
-    {
-        label: '语文',
-        key: '语文'
-    },
-    {
-        label: '数学',
-        key: '数学'
-    },
-    {
-        label: '英语',
-        key: '英语'
-    },
-    {
-        label: '物理',
-        key: '物理'
-    },
-    {
-        label: '化学',
-        key: '化学'
-    },
-    {
-        label: '历史',
-        key: '历史'
-    },
-    {
-        label: '地理',
-        key: '地理'
-    },
-    {
-        label: '生物',
-        key: '生物'
-    },
-    {
-        label: '政治',
-        key: '政治'
-    },
-    {
-        label: '微机',
-        key: '微机'
-    },
-    {
-        label: '劳动',
-        key: '劳动'
-    },
-    {
-        label: '班会',
-        key: '班会'
-    },
-    {
-        label: '体育',
-        key: '体育'
-    },
-    {
-        label: '自习',
-        key: '自习'
-    },
-    {
-        label: '其他',
-        key: '其他'
-    }
-])
+const lessonsOption = computed(() => {
+    let l = [
+        '语文', '数学', '英语', '物理', '化学',
+        '历史', '地理', '生物', '政治', '微机',
+        '劳动', '班会', '体育', '自习', '其他'
+    ]
+    return l.map(i => {
+        return { value: i, label: i }
+    })
+})
 
 function handlePatternSelect(key: number) {
     scheduleStore.schedule[props.day].pattern = key
@@ -120,14 +72,19 @@ function handlePatternSelect(key: number) {
     scheduleStore.schedule[props.day].lessons = lessons
 }
 
-/**
- * 课程选择
- * @param key 课程名称
- * @param id 第几节
-*/
-function handleLessonSelect(key: string, id: number) {
-    scheduleStore.schedule[props.day].lessons[id].name = key
-}
 </script>
 
-<style scoped></style>
+<style>
+.n-auto-complete .n-input-wrapper {
+    padding: 0 !important;
+}
+
+.n-auto-complete .n-input__border {
+    border: none !important;
+}
+
+.n-auto-complete .n-input-wrapper input {
+    font-size: 1.2rem !important;
+    text-align: center;
+}
+</style>
