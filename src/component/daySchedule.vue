d47fe095d1a79d0609f94a6eeefb8eba5c5ebd74
xwzkj <xwzkjbili@gmail.com>
2025-09-14 11:13:44 +0800

    add: 模式名编辑

--- src/component/daySchedule.vue
+++ src/component/daySchedule.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex flex-col items-center gap-1">
         <n-dropdown :options="scheduleStore.patternsOption" @select="handlePatternSelect" trigger="click">
-            <n-button dashed type="primary">{{ patternName }}</n-button>
+            <n-el tag="div" style="color: var(--primary-color);" class="cursor-pointer border b-dashed rounded-0.5rem p-t-0.1rem p-b-0.1rem">
+                <n-ellipsis class="w-5rem text-align-center text-0.8rem">{{ patternName }}</n-ellipsis>
+            </n-el>
         </n-dropdown>
         <div class="flex flex-col items-center gap-1" v-if="scheduleStore.schedule[props.day]?.pattern != -1">
             <div v-for="item in scheduleStore.schedule[props.day].lessons" class="w-100%">
@@ -12,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { NDropdown, NButton } from 'naive-ui'
+import { NDropdown, NEl, NEllipsis } from 'naive-ui'
 import { useScheduleStore } from '../stores/scheduleStore';
 import { computed } from 'vue';
 import ScheduleEditCard from './scheduleEditCard.vue'
