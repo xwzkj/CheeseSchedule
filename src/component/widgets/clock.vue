75097529827bf52a2568656d6a1af6c6d18baaf4
xwzkj <xwzkjbili@qq.com>
2025-10-03 09:26:18 +0800

    feat: 添加时钟小组件

---
+++ src/component/widgets/clock.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="h-3rem w-full bg-#fff rounded-1rem
+     text-2rem font-bold text-center
+     border-1px border-solid border-#ccc
+     flex items-center justify-center
+     overflow-hidden">
+        <div>{{ formattedTime }}</div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import dayjs from 'dayjs';
+
+const formattedTime = ref(dayjs().format('HH:mm:ss'));
+
+let timer = setInterval(() => {
+    formattedTime.value = dayjs().format('HH:mm:ss');
+}, 500);
+
+// 组件卸载时清除定时器
+onUnmounted(() => {
+    if (timer) {
+        clearInterval(timer);
+    }
+});
+</script>
+
+<style scoped></style>
