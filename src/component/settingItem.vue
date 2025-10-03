7ded09a9f2840f8611dd4044d45a3a63a0aabe2c
xwzkj <xwzkjbili@qq.com>
2025-10-03 12:11:14 +0800

    refactor: 删除未使用的导入

--- src/component/settingItem.vue
+++ src/component/settingItem.vue
@@ -1,36 +1,35 @@
 <template>
     <div class="w-full m-b-0.5rem">
-            <div class="flex justify-between bg-gray-500/5 
+        <div class="flex justify-between bg-gray-500/5 
             rounded-0.5rem p-1rem">
-                <div class="flex flex-col justify-center">
-                    <div class="text-1rem font-500">
-                        {{ props.t1 }}
-                    </div>
-                    <div class="text-0.8rem text-#666">
-                        {{ props.t2 }}
-                    </div>
+            <div class="flex flex-col justify-center">
+                <div class="text-1rem font-500">
+                    {{ props.t1 }}
                 </div>
-                <div class="flex items-center">
-                    <slot>
-                        <n-input v-model:value="inputValue" v-if="props.needInput" placeholder="请输入" />
-                        <n-button class="button" secondary circle type="primary">
-                            <template #icon>
-                                <n-icon>
-                                    <HugeiconsLinkSquare01 @click="props.actionOnClick" v-if="!props.needInput" />
-                                    <HugeiconsCheckmarkSquare01 @click="() => props.actionOnClick?.(inputValue)"
-                                        v-else />
-                                </n-icon>
-                            </template>
-                        </n-button>
-                    </slot>
+                <div class="text-0.8rem text-#666">
+                    {{ props.t2 }}
                 </div>
             </div>
+            <div class="flex items-center">
+                <slot>
+                    <n-input v-model:value="inputValue" v-if="props.needInput" placeholder="请输入" />
+                    <n-button class="button" secondary circle type="primary">
+                        <template #icon>
+                            <n-icon>
+                                <HugeiconsLinkSquare01 @click="props.actionOnClick" v-if="!props.needInput" />
+                                <HugeiconsCheckmarkSquare01 @click="() => props.actionOnClick?.(inputValue)" v-else />
+                            </n-icon>
+                        </template>
+                    </n-button>
+                </slot>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup name="settingItem" lang="ts">
 import { ref } from 'vue';
-import { NButton, NCard, NIcon, NInput } from 'naive-ui';
+import { NButton, NIcon, NInput } from 'naive-ui';
 import HugeiconsLinkSquare01 from '~icons/hugeicons/link-square-01';
 import HugeiconsCheckmarkSquare01 from '~icons/hugeicons/checkmark-square-01';
 
