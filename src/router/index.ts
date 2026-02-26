ce61ca30d17ffeb9380db09d3687c0f0d025f311
xwzkj <xwzkjbili@gmail.com>
2026-02-26 13:08:41 +0800

    refactor(pages): 统一页面文件名格式并更新路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,43 +10,43 @@ const router = createRouter({
         {
             name: 'editor',
             path: '/editor',
-            component: () => import('../pages/editorLayout.vue'),
+            component: () => import('../pages/editor-layout.vue'),
             children: [
                 {
                     name: 'editorHome',
                     path: '',
-                    component: () => import('../pages/editorHome.vue')
+                    component: () => import('../pages/editor-home.vue')
                 },
                 {
                     name: 'scheduleEditor',
                     path: 'schedule',
-                    component: () => import('../pages/scheduleEditor.vue')
+                    component: () => import('../pages/editor-schedule.vue')
                 },
                 {
                     name: 'patternEditor',
                     path: 'pattern',
-                    component: () => import('../pages/patternEditor.vue')
+                    component: () => import('../pages/editor-pattern.vue')
                 },
                 {
                     name: 'overrideEditor',
                     path: 'override',
-                    component: () => import('../pages/overrideEditor.vue')
+                    component: () => import('../pages/editor-override.vue')
                 },
                 {
                     name: 'drawEditor',
                     path: 'draw',
-                    component: () => import('../pages/drawEditor.vue')
+                    component: () => import('../pages/editor-draw.vue')
                 },
                 {
                     name: 'widgetEditor',
                     path: 'widget',
-                    component: () => import('../pages/widgetEditor.vue')
+                    component: () => import('../pages/editor-widget.vue')
 
                 },
                 {
                     name: 'setting',
                     path: 'setting',
-                    component: () => import('../pages/setting.vue')
+                    component: () => import('../pages/editor-setting.vue')
                 }
             ]
         },
