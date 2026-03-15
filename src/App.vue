da94751f22cd04f667c44d29f985bc1cd8dc8ab6
xwzkj <xwzkjbili@gmail.com>
2026-03-15 09:05:27 +0800

    refactor: 更改快捷键注册逻辑至app.vue

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,41 @@
     </n-config-provider>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { RouterView } from 'vue-router';
 import messageApi from './component/messageApi.vue';
 import { NMessageProvider, NConfigProvider, zhCN, dateZhCN } from 'naive-ui';
 import { ref } from 'vue';
 import { Window } from "@tauri-apps/api/window"
+import { isRegistered, register, unregisterAll } from '@tauri-apps/plugin-global-shortcut';
+import { emit } from "@tauri-apps/api/event";
 
 let themeOverrides = ref({
     common: {
         borderRadius: '0.5rem'
     },
 })
-try {
-    let currentWindow = Window.getCurrent()
-    if (currentWindow.label == 'cheese-schedule') { // 主窗口
-        currentWindow.onCloseRequested(() => {
-            currentWindow.preventDefault(); // 别关
-        })
+async function initAnyThing() {
+    try {
+        let currentWindow = Window.getCurrent()
+        if (currentWindow.label == 'cheese-schedule') { // 主窗口
+            currentWindow.onCloseRequested(() => {
+                (currentWindow as any).preventDefault(); // 别关
+            })
+        }
+        if (currentWindow.label == 'draw') { // 抽签窗口
+            // Ctrl+Alt+D 抽签快捷键  Ctrl+Alt+E 关闭抽签窗口快捷键
+            if (!(await isRegistered('CommandOrControl+Alt+D'))) {
+                await register('CommandOrControl+Alt+D', () => emit('draw'))
+                await register('CommandOrControl+Alt+E', () => emit('close-draw-window'))
+            }
+            window.addEventListener('beforeunload', () => unregisterAll())
+        }
+    } catch (error) {
+        console.error(error);
     }
-} catch (error) {
-    console.error("获取当前窗口失败:", error);
 }
+initAnyThing()
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,9 @@ import { createPinia } from 'pinia'
 import router from "./router/index";
 import 'virtual:uno.css'
 
-import { isRegistered, register, unregisterAll } from '@tauri-apps/plugin-global-shortcut';
-import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
-import { emit } from "@tauri-apps/api/event";
-
 const pinia = createPinia()
 
 createApp(App)
     .use(router)
     .use(pinia)
     .mount("#app");
-
-async function initShortcut() {
-    try {
-        if (getCurrentWebviewWindow().label == 'draw') {
-            // Ctrl+Alt+D 抽签快捷键  Ctrl+Alt+E 关闭抽签窗口快捷键
-            if(!(await isRegistered('CommandOrControl+Alt+D'))){
-                await register('CommandOrControl+Alt+D', () => emit('draw'))
-                await register('CommandOrControl+Alt+E', () => emit('close-draw-window'))
-            }
-            window.addEventListener('beforeunload', () => unregisterAll())
-        }
-    } catch (e) {
-        console.error(e)
-    }
-}
-initShortcut()
