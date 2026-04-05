22c7cbaa083211034e4b7053fb7630585dba749a
xwzkj <xwzkjbili@gmail.com>
2026-04-05 11:07:26 +0800

    feat: 修改提示词 换回3.5plus模型 优化历史记录逻辑

--- src/component/widgets/wordCard.vue
+++ src/component/widgets/wordCard.vue
@@ -51,35 +51,50 @@ let data = ref({ "word": "generating", "meaning": "AI生成中..." })
 onMounted(async () => {
     try {
         const completion = await (openai as any).chat.completions.create({
-            model: "qwen3.5-flash",
+            model: "qwen3.5-plus",
             // temperature: 1.3,
             thinking_budget: 150,
             messages: [
                 {
                     role: "system", content: `
     你是一个专业的英语教师，正在为同学准备一个单词卡片
-    ${scheduleStore.setting.widgetWordCardHistory.length > 0 ? `不要返回下面的单词：` : ``}
-    ${scheduleStore.setting.widgetWordCardHistory.join(', ')}
+    现在是${dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')}，你需要根据每天的日期输出不同的单词
+    今天的随机码是：${Math.floor(Math.random() * 1000000)}${Math.floor(Math.random() * 1000000)}${Math.floor(Math.random() * 1000000)}
+    根据以上信息返回词汇表中的单词，不要和其他随机码和时间生成的单词重复
+    今天要输出词典中第${Math.floor(Math.random() * 1000)}页的单词
+
+    ${scheduleStore.setting.widgetWordCardHistory.length > 0 ? `下列单词已经生成过，你这次**坚决不能**返回它们：` : ``}
+    ${scheduleStore.setting.widgetWordCardHistory.join(',')}
+    本次回复时严格避免生成以上的单词！
 
     ！！！重要！！！
-    你需要返回以下格式的JSON文本，包含word字段，是单词的原文；meaning字段是单词的中文释义，不能包含英文。meaning字段应为词性缩写（如n. v. adj. adv. prep.等）+汉语释义，多个含义用分号隔开，不同词性用空格隔开
+    你需要返回以下格式的JSON文本，包含：
+    word字段：单词的原文
+    meaning字段：单词的中文释义，不能包含英文。
+    
+    meaning字段应为词性缩写（如n. v. adj. adv. prep.等）+汉语释义。多个含义用分号隔开，如果有不同词性的释义用空格隔开
     例如：
+    有多种词性时：
     {"word": "record", "meaning": "n.记录;唱片 v.录制"}
     或者：
-    {"word": "smelly", "meaning": "adj. 有臭味的"}
-    务必按照以上格式返回，请勿返回其他内容，否则会导致程序崩溃。
-    可尽量简短每个字段的输出，过长后滚动显示观感欠佳
+    只有一种词性时：
+    {"word": "smelly", "meaning": "adj.有臭味的;有难闻气味的"}
+    务必按照以上格式返回，只能返回纯文本的json文本，请勿返回其他内容，否则会导致程序崩溃。
                 ` },
                 { role: "user", content: props.param.prompt.value }
             ],
         });
         data.value = JSON.parse(completion.choices[0].message.content ?? '{"word": "失败", "meaning": "AI返回错误"}')
         if (data.value.word != "失败") {
-            scheduleStore.setting.widgetWordCardHistory.push(data.value.word)
-            if (scheduleStore.setting.widgetWordCardHistory.length > 100) { // 防止数据过长
-                scheduleStore.setting.widgetWordCardHistory.splice(0, scheduleStore.setting.widgetWordCardHistory.length - 100)
+            if (!scheduleStore.setting.widgetWordCardHistory.includes(data.value.word)) {
+                scheduleStore.setting.widgetWordCardHistory.push(data.value.word)
+                if (scheduleStore.setting.widgetWordCardHistory.length > 100) { // 防止数据过长
+                    scheduleStore.setting.widgetWordCardHistory.splice(0, scheduleStore.setting.widgetWordCardHistory.length - 100)
+                }
+                scheduleStore.save(true)
+            }else{
+                console.error('生成了重复的单词', data.value.word)
             }
-            scheduleStore.save(true)
         }
     } catch (error) {
         data.value = { word: "失败", meaning: String(error) }
